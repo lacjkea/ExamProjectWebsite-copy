8fa3181674a81b42c9e943378e1a814150e67056
Juste Stankaityte <just1755@stud.kea.dk>
2022-05-31 15:26:29 +0200

    swiper and quantity selector js

--- productpage1.js
+++ productpage1.js
@@ -1,3 +1,4 @@
+//Related products swipper
 const productContainers = [...document.querySelectorAll('.relatedWrapper')];
 const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
 const preBtn = [...document.querySelectorAll('.pre-btn')];
@@ -14,3 +15,35 @@ productContainers.forEach((item, i) => {
         item.scrollLeft -= containerWidth;
     })
 })
+//Quantity selector and addint to cart
+
+window.onload=function(){
+    var minusBtn = document.getElementById("PPminus"),
+        plusBtn = document.getElementById("PPplus"),
+        numberPlace = document.getElementById("PPnumberPlace"),
+        submitBtn = document.getElementById("PPadd"),
+        number = 0, /// number value
+        min = 0, /// min number
+        max = 30; /// max number
+        
+    minusBtn.onclick = function(){
+        if (number>min){
+           number = number-1; /// Minus 1 of the number
+           numberPlace.innerText = number ; /// Display the value in place of the number
+           
+        }
+                
+    }
+    plusBtn.onclick = function(){
+        if(number<max){
+           number = number+1;
+           numberPlace.innerText = number ; /// Display the value in place of the number
+        }     
+     
+           
+    }
+    submitBtn.onclick = function(){
+        alert("Successfully added " + number + " bottles to your cart");
+    }
+    
+}
